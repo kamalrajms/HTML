f8d52f0873f30324c32be109004d21f5501aa62e
kamalrajms <kamalrajindtn@gmail.com>
2025-08-14 13:27:56 +0530

    variables

---
+++ script.js
@@ -0,0 +1,36 @@
+// console.log("hi");
+
+/* rhgig
+4h35h
+35history53h */
+
+var a = 55;
+console.log(a);
+var a = 44;
+a = 554;
+console.log(a);
+
+let b = 77;
+console.log(b);
+b = 99;
+console.log(b);
+
+const c = 10;
+console.log(c);
+console.log(typeof c);
+
+let name = "kamal";
+console.log(name);
+console.log(typeof name);
+
+let d = true;
+console.log(d);
+console.log(typeof d);
+
+let e = [];
+console.log(e);
+console.log(typeof e);
+
+let f;
+console.log(f);
+console.log(typeof f);
