3c34eaeb131efccc798d292bb3f03ec5b3a2de1f
kamalrajms <kamalrajindtn@gmail.com>
2025-08-21 11:02:47 +0530

    break,continue

--- script.js
+++ script.js
@@ -1,33 +1,73 @@
-for (let i = 1; i < 5; i++) {
+// for (let i = 1; i < 5; i++) {
+//   console.log(i);
+// }
+
+// let a = 1;
+// while (a <= 0) {
+//   console.log(a);
+//   a++;
+// }
+
+// let c = 0;
+// do {
+//   console.log(c);
+//   c++;
+// } while (c >= 5);
+
+//nested for
+
+// for (let i = 1; i <= 5; i++) {
+//   for (let j = 1; j <= 5; j++) {
+//     console.log(`${i} * ${j} = ${i * j}`);
+//   }
+//   console.log("----");
+// }
+
+// let a = 1;
+// while (a <= 5) {
+//   b = 1;
+//   let str = "";
+//   while (b <= 5) {
+//     str += b;
+//     b++;
+//     console.log(str);
+//   }
+//   a++;
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//   let str = "";
+//   for (let j = 1; j <= i; j++) {
+//     str += "*";
+//     console.log(str);
+//   }
+// }
+
+// //increment decrement
+// let x = 5;
+// let y = x++;
+// let z = x--;
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+// let A = 5;
+// let B = ++A;
+// let Z = --A;
+// console.log(A);
+// console.log(B);
+// console.log(Z);
+
+// for (let i = 1; i <= 6; i++) {
+//   if (i === 3) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+for (let i = 1; i <= 5; i++) {
+  if (i === 3) {
+    continue;
+  }
   console.log(i);
 }
-
-let a = 1;
-while (a <= 0) {
-  console.log(a);
-  a++;
-}
-
-let c = 0;
-do {
-  console.log(c);
-  c++;
-} while (c >= 5);
-
-// for (let i = 1; i < 5; i++) {
-//   for (let e = i; e <= i; e++) {
-//     console.log(e);
-//   }
-// }
-
-// let x = 0;
-// while (x <= 5) {
-//   while (x <= 2) {
-//     console.log(x * x);
-//   }
-//   console.log(x);
-
-//   x++;
-// }
-
-
