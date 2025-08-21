e411926f1821e7d8b690ecd3c2038c1123397784
kamalrajms <kamalrajindtn@gmail.com>
2025-08-21 11:47:02 +0530

    functions

--- script.js
+++ script.js
@@ -1,73 +1,48 @@
-// for (let i = 1; i < 5; i++) {
-//   console.log(i);
-// }
-
-// let a = 1;
-// while (a <= 0) {
-//   console.log(a);
-//   a++;
-// }
-
-// let c = 0;
-// do {
-//   console.log(c);
-//   c++;
-// } while (c >= 5);
-
-//nested for
-
-// for (let i = 1; i <= 5; i++) {
-//   for (let j = 1; j <= 5; j++) {
-//     console.log(`${i} * ${j} = ${i * j}`);
-//   }
-//   console.log("----");
-// }
-
-// let a = 1;
-// while (a <= 5) {
-//   b = 1;
-//   let str = "";
-//   while (b <= 5) {
-//     str += b;
-//     b++;
-//     console.log(str);
-//   }
-//   a++;
-// }
-
-// for (let i = 1; i <= 5; i++) {
-//   let str = "";
-//   for (let j = 1; j <= i; j++) {
-//     str += "*";
-//     console.log(str);
-//   }
-// }
-
-// //increment decrement
-// let x = 5;
-// let y = x++;
-// let z = x--;
-// console.log(x);
-// console.log(y);
-// console.log(z);
-
-// let A = 5;
-// let B = ++A;
-// let Z = --A;
-// console.log(A);
-// console.log(B);
-// console.log(Z);
-
-// for (let i = 1; i <= 6; i++) {
-//   if (i === 3) {
-//     break;
-//   }
-//   console.log(i);
-// }
-
-for (let i = 1; i <= 5; i++) {
-  if (i === 3) {
-    continue;
-  }
-  console.log(i);
+function greeting() {
+  console.log("Hello");
 }
+greeting();
+greeting();
+greeting();
+greeting();
+greeting();
+greeting();
+greeting();
+greeting();
+
+//parameter ,arguments
+
+function person(name) {
+  console.log("hi" + name);
+}
+person("SK");
+person("AK");
+
+//return value
+
+function numbers(a, b) {
+  return a + b;
+}
+
+let ans = numbers(3, 4);
+let ans1 = numbers(3, 5);
+let ans2 = numbers(3, 8);
+console.log(ans);
+console.log(ans1);
+console.log(ans2);
+
+// function Expression
+
+const multiply = function (a, b) {
+  return a * b;
+};
+console.log(multiply(4, 5));
+
+//arrow function
+
+const add = (a, b) => {
+  return a * b;
+};
+
+console.log(add(1, 1));
+
