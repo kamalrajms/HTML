b378865204f49082dc31bfe05b95c0de7dd5b87f
kamalrajms <kamalrajindtn@gmail.com>
2025-08-22 11:46:22 +0530

    array manipulating methods

--- script.js
+++ script.js
@@ -1,47 +1,21 @@
-let fruits = ["apple", "banana", "mangeo"];
-console.log(fruits);
-console.log(fruits[0]);
+let num = [2, 4, 5, 6, 71,2];
+let res = num.map((n, ind) => n * n);
+console.log(res);
 
-// Array decleration
-let A = new Array("A", "B", "c");
-console.log(A);
+let red = num.reduce((acc, current) => current + acc, 0);
+console.log(red);
 
-//array indexing
+let find = num.find((n) => n % 2 == 0);
+console.log(find);
 
-console.log(A[1]);
-console.log(A.length - 1);
+let filter = num.filter((e) => e % 2 === 0);
+console.log(filter);
 
-// Array methods
+let findindex = num.findIndex((e) => e >= 10);
+console.log(findindex);
 
-let color = ["black", "white", "red", "blue", "green"];
+let some = num.some((e) => e < 0);
+console.log(some);
 
-let pop = color.pop();
-console.log(pop);
-console.log(color);
-let push = color.push("pink");
-console.log(push);
-console.log(color);
-let shift = color.shift();
-console.log(shift);
-console.log(color);
-let unshift = color.unshift("violet");
-console.log(unshift);
-console.log(color);
-let slice = color.slice(1, 3);
-console.log(slice);
-console.log(color);
-let splice = color.splice(1, 3, "yellow", "skyblue");
-console.log(splice);
-console.log(color);
-console.log(color.indexOf("blue"));
-console.log(color.includes("red"));
-console.log(color.reverse());
-console.log(color.sort());
-
-// for (let i = 0; i < color.length; i++) {
-//   console.log(color[i]);
-// }
-
-// for (i of color) {
-//   console.log(i);
-// }
+let every = num.every((e) => e % 2 === 0);
+console.log(every);
